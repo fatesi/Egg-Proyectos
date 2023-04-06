9d372bf20acd0ef90adeabb607a29eb633d7db94
fatesi <edier.tellez@gmail.com>
2023-04-05 19:45:23 -0500

    final de dia

---
+++ JavaApplication2/src/Java1/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java1;
+
+import java.util.Scanner;
+
+
+
+
+
+/**
+ *
+ * @author edier
+ */
+public class NewClass {
+    public void NewClass(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+    }
+}
